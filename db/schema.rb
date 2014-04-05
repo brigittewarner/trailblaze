351d672a5123e70c6f659ba2ddce543ac7a25b19
Chris Guzman <guzmantchris@gmail.com>
2014-04-05 11:15:14 -0400

    Add User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140405035413) do
+ActiveRecord::Schema.define(version: 20140405145648) do
 
   create_table "badges_sashes", force: true do |t|
     t.integer  "badge_id"
@@ -75,6 +75,15 @@ ActiveRecord::Schema.define(version: 20140405035413) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "sash_id"
+    t.integer  "level",      default: 0
   end
 
 end
