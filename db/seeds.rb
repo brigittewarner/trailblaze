2f43276c71dfc947c5ccd1de49cf3fb5a793d42a
Brigitte Warner <brigitte.warner@gmail.com>
2014-04-05 14:09:50 -0400

    add trail marker seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
 healthy = Trail.create!(name: 'healthy', description: 'eat better, sleep better, and do yoga')
 spiritual = Trail.create!(name: 'spiritual', description: 'meditate, commune with nature')
 culinary = Trail.create!(name: 'culinary', description: 'learn to cook, make more meals at home')
+
+attend_yoga = Trailmarker.create!(name:'attend_yoga', description: "hot vinyasa baby!")
+meditate = Trailmarker.create!(name: 'meditate', description: "try out new meditation studio")
+acupuncture = Trailmarker.create!(name: 'acupuncture', description: "stick some needles in me")
