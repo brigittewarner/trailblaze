17c5eea4e7f3a443915c97480f4f51ba153941ad
Chris Guzman <guzmantchris@gmail.com>
2014-04-05 14:02:49 -0400

    user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
 healthy = Trail.create(name: 'healthy', description: 'eat better, sleep better, and do yoga')
 spiritual = Trail.create(name: 'spiritual', description: 'meditate, commune with nature')
 culinary = Trail.create(name: 'culinary', description: 'learn to cook, make more meals at home')
+
+chris = User.create(name: "Chris", email: "chris@example.com")
+
+chris.merit
