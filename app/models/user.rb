12b8f9a9943484df29b908191ef6e4e518330db7
Brandon Closson <bclosson@gmail.com>
2014-04-05 13:31:26 -0400

    added pg gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,26 @@
 class User < ActiveRecord::Base
   has_merit
 
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(strong_params)
+    if @user.save
+      flash[:success] = "User successfully created"
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def strong_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
