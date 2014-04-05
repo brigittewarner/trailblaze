350f36dbc072864ef3dcd4ccc8e8f858c2df4ae6
Brigitte Warner <brigitte.warner@gmail.com>
2014-04-05 13:54:39 -0400

    fix data bug

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
-healthy = Trail.create(name: 'healthy', description: 'eat better, sleep better, and do yoga')
-spiritual = Trail.create(name: 'spiritual', description: 'meditate, commune with nature')
-culinary = Trail.create(name: 'culinary', description: 'learn to cook, make more meals at home')
+healthy = Trail.create!(name: 'healthy', description: 'eat better, sleep better, and do yoga')
+spiritual = Trail.create!(name: 'spiritual', description: 'meditate, commune with nature')
+culinary = Trail.create!(name: 'culinary', description: 'learn to cook, make more meals at home')
