819789f47bc487e5c92310650a39d0e043e9d938
Brigitte Warner <brigitte.warner@gmail.com>
2014-04-05 14:56:54 -0400

    add trailmarkers view and def in controller

--- app/controllers/trailmarkers_controller.rb
+++ app/controllers/trailmarkers_controller.rb
@@ -8,4 +8,32 @@ class TrailmarkersController < ApplicationController
   def new
   	@trailmarker = Trailmarker.new
   end
+  def create
+		@trail = Trailmarker.new(trailmarker_params)
+		if @trailmarker.save
+			redirect_to trailmarkers_path, notice: "Trailmarker #{@trailmarker.name} was created"
+		else
+			render 'new'
+		end
+	end
+  def edit
+		@trailmarker = Trailmarker.find(params[:id])
+  end
+  def update
+		@trailmarker = Trailmarker.find(params[:id])
+		if @trailmarker.update(trailmarker_params)
+			redirect_to trailmarkers_path, notice: "Trailmarker #{@trailmarker.name} was updated"
+		else
+			render 'edit'
+		end
+	end
+	def destroy
+		@trailmarker = Trailmarker.find(params[:id])
+		@trailmarker.destroy
+		redirect_to trailmarkers_path, notice: "Trailmarker #{@trailmarker.name} was deleted"
+	end
+	protected
+	def trailmarker_params
+		params.require(:trailmarker).permit!
+	end
 end
--- app/controllers/trails_controller.rb
+++ app/controllers/trails_controller.rb
@@ -8,4 +8,32 @@ class TrailsController < ApplicationController
   def new
   	@trail = Trail.new
   end
+  def create
+		@trail = Trail.new(trail_params)
+		if @trail.save
+			redirect_to trails_path, notice: "Trail #{@trail.name} was created"
+		else
+			render 'new'
+		end
+	end
+  def edit
+		@trail = Trail.find(params[:id])
+  end
+  def update
+		@trail = Trail.find(params[:id])
+		if @trail.update(trail_params)
+			redirect_to trails_path, notice: "Trail #{@trail.name} was updated"
+		else
+			render 'edit'
+		end
+	end
+	def destroy
+		@trail = Trail.find(params[:id])
+		@trail.destroy
+		redirect_to trails_path, notice: "Trail #{@trail.name} was deleted"
+	end
+	protected
+	def trail_params
+		params.require(:trail).permit!
+	end
 end
