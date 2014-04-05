9544ce1f734379018b23120e2f3663aa489b2eeb
Chris Guzman <guzmantchris@gmail.com>
2014-04-05 00:10:03 -0400

    Fix bug

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Trailblaze::Application.routes.draw do
   resources :static_pages
   resources :trails
   resources :trailmarkers
-  
 
- # root :to => 'static_pages#index'
 
-  root :to => 'trails#new'
+  root :to => 'static_pages#index'
+
+  # root :to => 'trails#new'
 
 end
 
