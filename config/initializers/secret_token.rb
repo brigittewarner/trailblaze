3822c97ed8eca759cce692a781fbe90303ed2f4b
Brigitte Warner <brigitte.warner@gmail.com>
2014-04-04 22:03:24 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Trailblaze::Application.config.secret_key_base = 'e6b850ae556ef4a6e22ad3489508685ad6a8a9d50a38fd278c77345f9194c9ae57220b9d36cc655b23fb7682558fd0180d309e2103a09301da8cc2e42889afa7'
