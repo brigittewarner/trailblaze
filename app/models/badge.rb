9c4d5a36faa01b82a5c568f58008334918a32ce5
Brigitte Warner <brigitte.warner@gmail.com>
2014-05-05 19:35:41 -0400

    assign pioneer badge to user

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,4 +1,9 @@
 class Badge < ActiveRecord::Base
+  has_many :badgifications
   has_many :users, :through => :badgification
+
+  def assign_badge
+
+  end
 
 end
--- app/models/trail.rb
+++ app/models/trail.rb
@@ -1,6 +1,12 @@
 class Trail < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :initiation
-  has_many :trailmarkers, :through => :trailification
+  	has_many :trailmarkers, :through => :trailification
 
+  	after_create do |trail|
+  		puts "Congratulations, you have earned the Trailblazer Badge!"
+  		unless @current_user.id == Badgification.find(badge.id).user_id
+  			redirect_to @trail, success: "Your #{@trail.name} trail has been created!"
+  		end 		
+  	end
 end
