09f0f652a27954a8cc506d9c96a20b40faf85f84
Brigitte Warner <brigitte.warner@gmail.com>
2014-05-19 17:17:16 -0400

    Change badge copy

--- app/models/trail.rb
+++ app/models/trail.rb
@@ -4,7 +4,7 @@ class Trail < ActiveRecord::Base
   	has_many :trailmarkers, :through => :trailification
 
   	after_create do |trail|
-  		puts "Congratulations, you have earned the Trailblazer Badge!"
+  		puts "Congratulations, you have earned the Pioneer Badge!"
   		unless @current_user.id == Badgification.find(badge.id).user_id
   			redirect_to @trail, success: "Your #{@trail.name} trail has been created!"
   		end 		
